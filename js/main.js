0fff8441cd8abf00c1c1d1e5f89e56f5834f2d99
thisisyoungjun <122899@naver.com>
2022-05-12 15:30:42 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// header.html include
+$(document).ready(function (){
+    $("#header_title").load("layout/header.html");
+})
+
+//header_title event
+const header_title=document.getElementsByClassName('header_title');
+
+console.log()
+
