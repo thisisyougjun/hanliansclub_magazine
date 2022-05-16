e64bb65e9178673e2b6e81f1ad686acc6226e190
thisisyoungjun <122899@naver.com>
2022-05-16 23:00:53 +0900

    22-05-16 p.m 23:00

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 $(document).ready(function (){
     $("#header_title").load("layout/header.html");
 })
+//footer include
+$(document).ready(function (){
+    $("#footer").load("layout/footer.html");
+})
 
 //header_title event
 const header_title=document.getElementsByClassName('header_title');
